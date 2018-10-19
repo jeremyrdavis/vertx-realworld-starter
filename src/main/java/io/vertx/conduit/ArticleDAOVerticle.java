7f1fda473d421591af961e6dda9b06400fc9d750
Ram Maddali <ram.maddali@gmail.com>
2018-10-19 13:49:58 -0400

    Update ArticleDAOVerticle.java

--- src/main/java/io/vertx/conduit/ArticleDAOVerticle.java
+++ src/main/java/io/vertx/conduit/ArticleDAOVerticle.java
@@ -29,13 +29,7 @@ public class ArticleDAOVerticle extends AbstractVerticle{
 	  
 	  private static final String URI = "mongodb://127.0.0.1:27017";
 	  private static final String DB="mydb";
-	/*
-	  Convenience method so you can run it in your IDE
-	   */
-	  public static void main(String[] args) {
-	   runExample(ArticleDAOVerticle.class);
-	  }
-	  
+	
 	  
 	  
 	  
@@ -286,59 +280,6 @@ public class ArticleDAOVerticle extends AbstractVerticle{
 	  
 	
 	
-	  public static void runExample(Class clazz) {
-		    runExample(MONGO_EXAMPLES_JAVA_DIR, clazz, new VertxOptions(), null);
-		  }
-	  public static void runExample(String exampleDir, Class clazz, VertxOptions options, DeploymentOptions
-		      deploymentOptions) {
-		    runExample(exampleDir + clazz.getPackage().getName().replace(".", "/"), clazz.getName(), options, deploymentOptions);
-		  }
-	
-	  public static void runExample(String exampleDir, String verticleID, VertxOptions options, DeploymentOptions deploymentOptions) {
-		    if (options == null) {
-		      // Default parameter
-		      options = new VertxOptions();
-		    }
-		    // Smart cwd detection
-
-		    // Based on the current directory (.) and the desired directory (exampleDir), we try to compute the vertx.cwd
-		    // directory:
-		    try {
-		      // We need to use the canonical file. Without the file name is .
-		      File current = new File(".").getCanonicalFile();
-		      if (exampleDir.startsWith(current.getName()) && !exampleDir.equals(current.getName())) {
-		        exampleDir = exampleDir.substring(current.getName().length() + 1);
-		      }
-		    } catch (IOException e) {
-		      // Ignore it.
-		    }
-
-		    System.setProperty("vertx.cwd", exampleDir);
-		    Consumer<Vertx> runner = vertx -> {
-		      try {
-		        if (deploymentOptions != null) {
-		          vertx.deployVerticle(verticleID, deploymentOptions);
-		        } else {
-		          vertx.deployVerticle(verticleID);
-		        }
-		      } catch (Throwable t) {
-		        t.printStackTrace();
-		      }
-		    };
-		    if (options.isClustered()) {
-		      Vertx.clusteredVertx(options, res -> {
-		        if (res.succeeded()) {
-		          Vertx vertx = res.result();
-		          runner.accept(vertx);
-		        } else {
-		          res.cause().printStackTrace();
-		        }
-		      });
-		    } else {
-		      Vertx vertx = Vertx.vertx(options);
-		      runner.accept(vertx);
-		    }
-		  }
-	
+	 
 	
 }
